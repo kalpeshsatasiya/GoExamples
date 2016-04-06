69e1e68358d2c871ddbd2a587d0aa9298d6b2403
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-04-06 22:11:53 +0530

    Modification for code Formatting

--- VariadicFunctions.go
+++ VariadicFunctions.go
@@ -16,6 +16,7 @@ func main() {
 	sum(1, 2)
 	sum(1, 2, 3)
 
+
 	//If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this.
 	nums := []int{1, 2, 3, 4}
 	sum(nums ...)
