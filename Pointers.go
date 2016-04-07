22e45c99222ae03ea83294e9f620b5a6fbeb4ef2
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-04-07 23:18:12 +0530

    Few modification for code refactoring

--- Pointers.go
+++ Pointers.go
@@ -11,13 +11,16 @@ func zeroval(ival int) {
 //zeroptr in contrast has an *int parameter, meaning that it takes an int pointer.
 // The *iptr code in the function body then dereferences the pointer from its memory address to the current value at that address.
 // Assigning a value to a dereferenced pointer changes the value at the referenced address.
-func zeroprt(iptr *int)  {
+
+func zeroprt(iptr *int) {
 	*iptr = 0
 }
 func main() {
+
 	//The &i syntax gives the memory address of i, i.e. a pointer to i.
+
 	i := 1
-	fmt.Println("Inital :" , i)
+	fmt.Println("Inital :", i)
 
 	zeroval(i)
 	fmt.Println("zeroval:", i)
--- Slices.go
+++ Slices.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
+
 	//Unlike arrays, slices are typed only by the elements they contain (not the number of elements).
 	// To create an empty slice with non-zero length, use the builtin make. Here we make a slice of strings of length 3 (initially zero-valued).
+
 	s := make([]string, 3)
 	fmt.Println("emp:", s)
 
@@ -58,6 +60,7 @@ func main() {
 			twoD[i][j] = i + j
 		}
 	}
+
 	fmt.Println("2d", twoD)
 }
 
